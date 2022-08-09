d14bcf3e6817f7bcd437ffb2ae53f28d792f0c96
jiksonsabu <jiksonks1@gmail.com>
2022-08-09 14:14:39 +0530

    2

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
+
 const express = require('express');
 const path = require('path');
 const app = express();
-
-app.use(express.static(__dirname + '/dist/myapp'));
-
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/myapp/index.html'));
-});
-
+app.use(express.static(__dirname + '/dist/myapp1'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/myapp1/index.html'));});
 app.listen(process.env.PORT || 8080);
